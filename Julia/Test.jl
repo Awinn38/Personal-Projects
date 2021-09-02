007588e71f766f748d78ca09380593bc6f4aea95
Antonio Winn <awinn2@umbc.edu>
2021-09-02 14:44:28 -0400

    A boatload of changes for Jupyter and Julia

--- Julia/Test.jl
+++ Julia/Test.jl
@@ -1,3 +1,27 @@
-using Plots
-pyplot()
-plot(rand(4,4))
+# using Plots
+# pyplot()
+# plot(rand(4,4))
+
+# import Pkg; Pkg.add("DifferentialEquations")
+# using DifferentialEquations
+# f(u,p,t) = 1.01*u
+# u0 = 1/2
+# tspan = (0.0,1.0)
+# prob = ODEProblem(f,u0,tspan)
+# sol = solve(prob,Tsit5())
+
+# Pkg.add("Optim")
+# using Optim
+# rosenbrock(x) =  (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
+# result = optimize(rosenbrock, zeros(2), BFGS())
+
+# x = symbols("x")
+# SymPy.simplify(sin(x)^2 + cos(x)^2)	
+# SymPy.factor(x^2 + 2x+ 1)
+
+# using CalculusWithJulia
+# using Plots
+# plotly()
+
+# position(t) = 60*t
+# plot(position,0,3)
--- Python/starterFiles/Class_test.py
+++ Python/starterFiles/Class_test.py
@@ -1,8 +1,18 @@
 class TestClass:
-    def test_one(self):
+    def test_one():
         x = "edureka"
         assert 'r' in x
+        return print(x)
 
-    def test_two(self):
+    def test_two():
         x = 'hello'
         assert hasattr(x,'check')
+
+# def student(firstname,lastname = 'Mark', standard = 'Fifth'):
+    
+#     print(firstname,lastname,'studies in',standard,'Standard')
+
+# Antonio = 'Antonio'
+# student(Antonio)
+
+TestClass.test_one()
