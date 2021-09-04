a71ef76e451bc1d2132231cf63c691ea9dc33dc7
Antonio Winn <awinn2@umbc.edu>
2021-09-04 02:30:40 -0400

    Worked on separations Hw 1

--- Julia/ENCH445/Problem_1-1.jl
+++
@@ -1,9 +0,0 @@
-# x,y = symbols("x,y")
-# Eqn_1 = 2*x*y - 2*x^)2 + 4*sin(y) + 4
-# Eqn_2 = 3*x^2 - 2*x*y^2 + 5*cos(x) + 4
-# convert(AbstractFloat,Eqn_1(1,1))
-function mysqrt(x)
-    x < 0 && throw(DomainError("x is negative"))
-    x^0.5
-end
-mysqrt(1)
--- Python/starterFiles/Test2.py
+++ Python/starterFiles/Test2.py
@@ -1,4 +1,5 @@
 from gekko import GEKKO
+
 m=GEKKO()
 p=m.Param(1.2)
 x=m.Array(m.Var,3)
@@ -7,4 +8,4 @@ eq1 = x[2]-1==x[1]+x[0]
 m.Equation(x[2]==x[1]**2)
 m.Equations([eq0,eq1])
 m.solve()
-print(x[2].value)
+print([x[2].value,x[1].value,x[0].value])
