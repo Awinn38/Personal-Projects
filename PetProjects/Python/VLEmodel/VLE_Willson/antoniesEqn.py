03b4f092fc35b75499b7156e55902b58161fdfe8
Antonio Winn <awinn2@umbc.edu>
2021-11-15 20:20:00 -0500

    Work update

--- PetProjects/Python/VLEmodel/VLE_Willson/antoniesEqn.py
+++ PetProjects/Python/VLEmodel/VLE_Willson/antoniesEqn.py
@@ -3,3 +3,9 @@ PsatMeth = (
     10**((7.89750)-((1474.08) / (229.13+(ConstantsWilson.T_Willson-ConstantsWilson.Tref_Willson))))) / 7.5006
 PsatWate = (
     10**((8.01195)-((1698.785) / (231.04+(ConstantsWilson.T_Willson-ConstantsWilson.Tref_Willson))))) / 7.5006
+
+### Design Project 
+PsatH20 = (
+    10**((3.55959)-((643.748) / (460+(-198.043))))) / 7.5006
+PsatPropyleneGlycol = (
+    10**((6.07936)-((2692.187) / (460+(-17.94))))) / 7.5006
